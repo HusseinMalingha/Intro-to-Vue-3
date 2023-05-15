9c2ca22f79bf6c96460a1665f1713fc9df165839
HusseinMalingha <husseinmalingha@gmail.com>
2023-05-15 19:44:13 +0300

    added variants in vue fuctionality

--- main.js
+++ main.js
@@ -4,7 +4,14 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id:1, size:'sm'},
+                {id:2, size:'m'},
+                {id:3, size:'l'},
+                {id:4, size:'x'},
+                {id:5, size:'xl'}
+            ]
         }
     }
 })
